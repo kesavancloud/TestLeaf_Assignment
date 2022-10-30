112fa7101a56a179cee4de25b49fbda01752d998
kesavancloud <116991786+kesavancloud@users.noreply.github.com>
2022-10-30 09:50:38 +0530

    Add files via upload

---
+++ day1/Bike.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		
+		Car obj=new Car();
+		obj.applyBreak();
+		obj.soundHorn();
+	}
+	
+		
+}
---
+++ day1/Car.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Car {
+
+	public void applyBreak() {
+		
+		System.out.println("Applied break");
+
+	}
+	public void soundHorn() {
+	
+		System.out.println("Sound Horn");
+	}
+}
